0bb1fb6d1943e4540ec0666263cbe4cb76ef71c6
Cool <mo9.no4ta.dz@gmail.com>
2024-02-27 12:42:07 +0300

    123

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:idi_rabotai123/database/firebaseAuth/user.dart';
+import 'package:idi_rabotai123/ui/home.dart';
+import 'package:idi_rabotai123/ui/pages/authorization.dart';
 import 'package:provider/provider.dart';
 
 class LandingPage extends StatelessWidget {
@@ -8,5 +10,7 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final UserModel? userModel = Provider.of<UserModel?>(context);
+    final bool check = userModel != null ? true : false;
+    return check ? const HomePage() : const AuthorizationPage();
   }
 }
