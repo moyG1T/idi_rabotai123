dea6123c61444b5dfb4407d29edf5a4bcb3ea148
Cool <mo9.no4ta.dz@gmail.com>
2024-02-27 12:49:07 +0300

    111

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:idi_rabotai123/routes/routes.dart';
 import 'package:idi_rabotai123/themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDXeH2Y1cLDUu0NTwziq9Nco1tYF4JAQDs",
+          appId: appId,
+          messagingSenderId: messagingSenderId,
+          projectId: projectId));
+
   runApp(const MainApp());
 }
 
