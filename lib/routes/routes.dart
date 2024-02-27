2aae8f35ae798d1152ec835c5c17c5488451939f
Cool <mo9.no4ta.dz@gmail.com>
2024-02-27 12:44:01 +0300

    321

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,11 @@
+import 'package:idi_rabotai123/landing.dart';
 import 'package:idi_rabotai123/ui/home.dart';
 import 'package:idi_rabotai123/ui/pages/authorization.dart';
 import 'package:idi_rabotai123/ui/pages/registration.dart';
 
 final routes = {
-  '/': (context) => const AuthorizationPage(),
+  '/': (context) => const LandingPage(),
+  '/auth': (context) => const AuthorizationPage(),
   '/reg': (context) => const RegistrationPage(),
   '/home': (context) => const HomePage(),
 };
